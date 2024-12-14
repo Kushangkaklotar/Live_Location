7a509026e5d8da51258ac02359ca62be542107f7
Kushang <164315015+kushang-iroid@users.noreply.github.com>
2024-12-15 00:12:39 +0530

    Create home page

---
+++ LiveLocationTrack/Screen/Location/HomeScreen.swift
@@ -0,0 +1,31 @@
+//
+//  HomeScreen.swift
+//  LiveLocationTrack
+//
+//  Created by Kushang  on 15/12/24.
+//
+
+import UIKit
+
+class HomeScreen: UIViewController {
+    // MARK: - IB Outlet
+    
+    // MARK: - Variables
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - Function's
+    
+    // MARK: - IB  Actoin method
+    
+}
+
+// MARK: - Extension
+
+// MARK: - Delegate method
+
+// MARK: - API Call
